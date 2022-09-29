c86b8e244563dde3911a857eb66c6a63045f75b5
Ellie Chapman <ellie@amadido.co.uk>
2022-09-29 15:19:14 +0100

    started lexer

---
+++ lexer.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <cassert>
+
+enum Kind { NoMoreToken, UnknownCharacter, Identifier, Plus, LP, RP };
+
+#define kind_to_string(E) case E: return #E
+
+std::string showKind(Kind k);
+
+class Token {
+private:
+  std::string _text;
+  Kind _kind;
+  unsigned int _start_pos;
+  unsigned int _end_pos;
+public:
+  Token(std::string text, Kind kind, unsigned int start_pos, unsigned int end_pos) : _text(text), _kind(kind), _start_pos(start_pos), _end_pos(end_pos) {}
+  std::string text() { return _text; };
+  Kind kind() { return _kind; };
+  unsigned int start_pos() { return _start_pos; };
+  unsigned int end_pos() { return _end_pos; };
+};
+
+class LexState {
+private:
+  std::string _whole_input;
+  unsigned _index;
+public:
+  LexState(std::string input) : _whole_input(input), _index(0) {};
+  Token get_token();
+};
